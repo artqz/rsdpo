6b75934417ee9688969e16d67ad1be46644f4880
artqz <djoctuk@yandex.ru>
2018-05-18 11:00:20 +0500

    update navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -17,6 +17,27 @@
                 <li class="active"><a class="nav-link" href="{{ url('/') }}">Главная</a></li>
                 <li><a class="nav-link" href="{{ url('/') }}">О сервисе</a></li>
             </ul>
+            @if(!Auth::guest())
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{Auth::user()->name}} <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        @if(Auth::user()->role_id == 1 | Auth::user()->role_id == 2)
+                        <li><a href="{{ url('/admin') }}" style="color: red;"><i class="fa fa-cog" aria-hidden="true"></i> Панель управления</a></li>
+                        @endif
+                        <li><a href="{{ url('/base') }}"><i class="fa fa-book"></i> Учебная база</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                Выйти
+                            </a>
+                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            @endif
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
